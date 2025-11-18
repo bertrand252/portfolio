1c09d4ff59c79a437f008c1cbd12cf3ab9a4c730
bertrand252 <bertrandleonard736@gmail.com>
2025-11-19 00:17:19 +0800

    fix cv bug

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,7 @@ function App() {
             that keep me balanced and inspired to create even more.
           </p>
           <div className="flex justify-center lg:justify-start">
-            <a href="CV.pdf" download="CV.pdf">
+            <a href="./public/assets/CV.pdf" download="CV.pdf">
               <button
                 type="button"
                 className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-xs sm:text-sm px-3 sm:px-5 py-2 sm:py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
